72c3f41f4e9bfe6a70b214b7360092328a7be4f5
hiroshi.yamazaki <hiroshi.yamazaki@jp.oro.com>
2015-06-04 21:24:59 +0900

    add

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ A high-productivity web framework for the [Go language](http://www.golang.org/).
 
 ### Start the web server:
 
-    revel run myapp
+    revel run svnadmin
 
    Run with <tt>--help</tt> for options.
 
-### Go to http://localhost:9000/ and you'll see:
+### Go to http://localhost:28080/ and you'll see:
 
 "It works"
 
--- app/controllers/svn.go
+++ app/controllers/svn.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"svnadmin/app/routes"
+	"regexp"
 )
 
 type Svn struct {
@@ -29,20 +30,27 @@ func (c Svn) Index() revel.Result {
 		repos[i] = filepath.Base(path)
 	}
 
-	return c.Render(repos)
+	svn_url_base := revel.Config.StringDefault("svn.url", "http://xxxxxxxxx/")
+	
+	return c.Render(repos, svn_url_base)
 }
 
 func (c Svn) Create(Name string) revel.Result {
 	repo := c.getSvnParentPath() + "/" + Name
-
+		
 	_, e := os.Stat(repo)
 	is_exists := false
 	if e != nil {
 		is_exists = true
 	}
-
-	c.Validation.Required(Name).Message("Name must input")
-	c.Validation.Required(is_exists).Message("Name already exist")
+	
+	re := regexp.MustCompile("^[a-z0-9_.-]+$")
+	
+	c.Validation.Required(Name).Message("リポジトリ名は必須です。")
+	c.Validation.Required(Name != "websvnadmin").Message("websvnadminは予約語のた指定できません。")
+	c.Validation.Required(Name != "svn").Message("svnは予約語のた指定できません。")
+	c.Validation.Required(is_exists).Message("入力のリポジトリ名はすでに存在しています。")
+	c.Validation.Match(Name, re).Message("リポジトリ名は半角英数記号で指定してください。")
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
@@ -53,14 +61,18 @@ func (c Svn) Create(Name string) revel.Result {
 	if err != nil {
 		c.FlashParams()
 		fmt.Println(err)
+		
+		c.Flash.Error(fmt.Sprintf("%sの作成に失敗しました。", Name))
 		return c.Redirect(routes.Svn.Index())
 	}
 
 	owner := revel.Config.StringDefault("svn.owner", "apache")
 	group := revel.Config.StringDefault("svn.group", "apache")
-
+	permit := revel.Config.StringDefault("svn.permit", "775")
+	
 	exec.Command("sudo", "chown", "-R", fmt.Sprintf("%s.%s", owner, group), repo).Run()
-	exec.Command("sudo", "chmod", "-R", "775", repo).Run()
+	exec.Command("sudo", "chmod", "-R", permit, repo).Run()
 
+	c.Flash.Success(fmt.Sprintf("%sを作成しました。", Name))
 	return c.Redirect(routes.Svn.Index())
 }
